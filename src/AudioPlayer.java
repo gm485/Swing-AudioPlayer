8e1987cc5504e62ac71a505966ef2f0c659cc998
BeanoMn <gm485@github.com>
2024-02-29 10:43:21 +0000

    Presets class created

---
+++ src/AudioPlayer.java
@@ -0,0 +1,19 @@
+//Project 3-1: A Swing interface for an audio player
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+import javax.swing.event.*;
+import java.text.*;
+import java.util.*;
+
+// A class that holds the audio settings.
+class Presets {
+    int bass, midrange, treble, balance, volume;
+    Presets(int b, int m, int t, int bl, int v) {
+        bass = b; midrange =m; treble = t; balance =bl; volume = v;
+    }
+}
+//the main audio interface class.
+class AudioPlayer {
+
+}
